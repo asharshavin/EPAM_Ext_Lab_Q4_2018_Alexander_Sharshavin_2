7eb37df62505830bdaa718af22f1381a47b64313
alexandr.sharshavin <8593228@mail.com>
2018-12-08 07:51:27 +0400

    добавлены задания 3.7-3.13

--- 3.Basic C/ConsoleApp/ConsoleApp/Program.cs
+++ 3.Basic C/ConsoleApp/ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace ConsoleApp
         {
             //Создание объекта для генерации чисел
             Random rnd = new Random();
-            int x = 6;// rnd.Next(1, 13);
+            int x = 1;// rnd.Next(1, 13);
             string str = "";
             do
             {
@@ -38,7 +39,14 @@ namespace ConsoleApp
                         case 4: Task4_Elka_Elka.DoIt(); break;
                         case 5: Task5_Summa.DoIt(); break;
                         case 6: Task6_Shrift.DoIt(); break;
-                        default: Console.WriteLine("задание не выполнено!");  break;
+                        case 7: Task7_Sort.DoIt(); break;
+                        case 8: Task8_3dArray.DoIt(); break;
+                        case 9: Task9_1dArray.DoIt(); break;
+                        case 10: Task10_2dArray.DoIt(); break;
+                        case 11: Task11_AveragLenthOfWord.DoIt(); break;
+                        case 12: Task12_DoubleTrouble.DoIt(); break;
+                        case 13: Task13_VincentAndJules.DoIt(); break;
+                        default: Console.WriteLine("задание отсутствует!");  break;
                     }
                 }
             } while (str != null || x != 0);
@@ -295,6 +303,7 @@ class Task5_Summa
     {
         int[] b = { 3, 5 };
         int sum = 0;
+
         //Создание объекта для генерации чисел
         Random rnd = new Random();
 
@@ -355,10 +364,10 @@ class Task6_Shrift
     [Flags]
     public enum Shrift
     {
-        bold = 0x01,
-        italic = 0x02,
-        underline = 0x04,
-        none = 0x00,
+        bold = 0x01, //жирный 
+        italic = 0x02, //курсив
+        underline = 0x04, //подчеркивание
+        none = 0x00, //ничего
     }
     public static void DoIt()
     {
@@ -385,104 +394,309 @@ class Task6_Shrift
                 default: break;
             }
         } while (str != null || str != "");
-
-
-
 } }
 
 
-//class Task7_Sort
-//{
-//    public static void DoIt()
-//    {
-//        const int length = 5;
-//        int array[length] = { 30, 50, 20, 10, 40 };
+class Task7_Sort
+{
+    static void Print(int[] a)
+    {
+        foreach (int elem in a)
+            Console.Write("{0,3} ", elem);
+    }
+    static void FillRandom(int[] a)
+    {
+        Random rnd = new Random();
 
-//        // Перебираем каждый элемент массива
-//        // (кроме последнего, он уже будет отсортирован к тому времени, когда мы до него доберемся)
-//        for (int const int length = 5;
-//        int array[length] = { 30, 50, 20, 10, 40 };
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            a[i] = rnd.Next(-99, 99);
+        }
+    }
 
-//        // Перебираем каждый элемент массива
-//        // (кроме последнего, он уже будет отсортирован к тому времени, когда мы до него доберемся)
-//        for (int startIndex = 0; startIndex < length - 1; ++startIndex)
-//        {
-//            // В переменной smallestIndex хранится индекс наименьшего значения, которое мы нашли в этой итерации
-//            // Начинаем с того, что наименьший элемент в этой итерации - это первый элемент (индекс 0)
-//            int smallestIndex = startIndex;
+    public static void DoIt()
+    {
+        //инициализируем генератор случайных чисел
+        int[] myArray = new int[10];
+        FillRandom(myArray);
+        Console.WriteLine("Первоначальный массив:");
+        Print(myArray);
+        Sort(myArray);
+        Console.WriteLine("Отсортированный массив: ");
+        Print(myArray);
+        Console.WriteLine();
+    }
 
-//            // Затем ищем элемент поменьше в остальной части массива
-//            for (int currentIndex = startIndex + 1; currentIndex < length; ++currentIndex)
-//            {
-//                // Если мы нашли элемент, который меньше нашего наименьшего элемента
-//                if (array[currentIndex] < array[smallestIndex])
-//                    // запоминаем его
-//                    smallestIndex = currentIndex;
-//            }
+    static void Sort(int[] a)
+    {
+        int temp;
+        for (int i = 1; i < a.Length; i++)
+        {
+            int j = i;
+            while (j >= 1 && a[j] < a[j - 1]) //1
+            {
+                temp = a[j];
+                a[j] = a[j - 1];
+                a[j - 1] = temp;
+                j--;
+            }
+        }
+    }
+}
 
-//            // smallestIndex теперь наименьший элемент 
-//            // меняем местами наше начальное наименьшее число с тем, которое мы обнаружили
-//            std::swap(array[startIndex], array[smallestIndex]);
-//        }
+class Task8_3dArray
+{
+    static void Print(int[,,] a)
+    {
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+                for (int k = 0; k < a.GetLength(2); k++)
+                {
+                    Console.Write("{0,3} ", a[i, j, k]);
+                }
+                Console.Write("\t ");
+            }
+            Console.WriteLine();
+        }
+    }
+    static void FillRandom(int[,,] a)
+    {
+        Random rnd = new Random();
 
-//        // Теперь, когда весь массив отсортирован - выводим его на экран
-//        for (int index = 0; index < length; ++index)
-//            std::cout << array[index] << ' ';
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+                for (int k = 0; k < a.GetLength(2); k++)
+                {
+                    a[i, j, k] = rnd.Next(-99, 99);
+                }
+            }
+        }
+    }
 
-//        return 0; = 0; startIndex < length - 1; ++startIndex)
-//        {
-//            // В переменной smallestIndex хранится индекс наименьшего значения, которое мы нашли в этой итерации
-//            // Начинаем с того, что наименьший элемент в этой итерации - это первый элемент (индекс 0)
-//            int smallestIndex = startIndex;
+    static void ChangePositive(int[,,] a)
+    {
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+                for (int k = 0; k < a.GetLength(2); k++)
+                {
+                    if (a[i, j, k] > 0) a[i, j, k] = 0;
+                }
+            }
+        }
+    }
 
-//            // Затем ищем элемент поменьше в остальной части массива
-//            for (int currentIndex = startIndex + 1; currentIndex < length; ++currentIndex)
-//            {
-//                // Если мы нашли элемент, который меньше нашего наименьшего элемента
-//                if (array[currentIndex] < array[smallestIndex])
-//                    // запоминаем его
-//                    smallestIndex = currentIndex;
-//            }
 
-//            // smallestIndex теперь наименьший элемент 
-//            // меняем местами наше начальное наименьшее число с тем, которое мы обнаружили
-//            std::swap(array[startIndex], array[smallestIndex]);
-//        }
+    public static void DoIt()
+    {
+        //инициализируем генератор случайных чисел
+        int[,,] myArray = new int[10, 10, 10];
+        FillRandom(myArray);
+        Console.WriteLine("Первоначальный массив:");
+        Print(myArray);
+        ChangePositive(myArray);
+        Console.WriteLine("После замены положительных на нули:");
+        Print(myArray);
+    }
+}
 
-//        // Теперь, когда весь массив отсортирован - выводим его на экран
-//        for (int index = 0; index < length; ++index)
-//            std::cout << array[index] << ' ';
+class Task9_1dArray
+{
+    static void Print(int[] a)
+    {
+        foreach (int elem in a)
+            Console.Write("{0,3} ", elem);
+    }
+    static void FillRandom(int[] a)
+    {
+        Random rnd = new Random();
 
-//        return 0;
-//    }
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            a[i] = rnd.Next(-99, 99);
+        }
+    }
 
-//    private static int Parse(string str, int x)
-//    {
-//        if (str == null) return -1;
-//        if (str == "")
-//        {
-//            //Console.WriteLine(x);
-//            return x;
-//        };
-//        try
-//        {
-//            x = int.Parse(str);
-//        }
-//        catch (FormatException)
-//        {
-//            Console.WriteLine("Неверный ввод!");
-//            return -1;
-//        }
-//        if (x < 0)
-//        {
-//            Console.WriteLine("Число не может быть меньше 0!");
-//            return -1;
-//        }
-//        if (x > 1000)
-//        {
-//            Console.WriteLine("Число не может быть больше 1000!");
-//            return -1;
-//        }
-//        return x;
-//    }
-//}
+    static long SummaPositive(int[] a)
+    {
+        long sum = 0;
+
+        foreach (int elem in a)
+            if (elem > 0) sum += elem;
+
+        return sum;
+    }
+
+
+    public static void DoIt()
+    {
+        //инициализируем генератор случайных чисел
+        int[] myArray = new int[10];
+        FillRandom(myArray);
+        Console.WriteLine("Первоначальный массив:");
+        Print(myArray);
+        Console.WriteLine("Сумма положительных элементов массива: {0}", SummaPositive(myArray));
+    }
+}
+
+class Task10_2dArray
+{
+    static void Print(int[,] a)
+    {
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+                 Console.Write("{0,3} ", a[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+    static void FillRandom(int[,] a)
+    {
+        Random rnd = new Random();
+
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+                a[i, j] = rnd.Next(-99, 99);
+            }
+        }
+    }
+
+    static long SummaEvenIndexes(int[,] a)
+    {
+        long sum = 0;
+
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+                if ((i+j) % 2 == 0) sum += a[i, j];
+            }
+        }
+        return sum;
+    }
+
+
+    public static void DoIt()
+    {
+        //инициализируем генератор случайных чисел
+        int[,] myArray = new int[4, 4];
+        FillRandom(myArray);
+        Console.WriteLine("Первоначальный массив:");
+        Print(myArray);
+        Console.WriteLine("Сумма элементов массива с четной суммой индексов: {0}", SummaEvenIndexes(myArray));
+    }
+}
+
+class Task11_AveragLenthOfWord
+{
+    public static void DoIt()
+    {
+        string poems = "Написать программу, которая определяет среднюю длину слова во введенной текстовой строке. Учесть, что символы пунктуации на длину слов влиять не должны. Регулярные выражения не использовать. И не пытайтесь прописать все ручками. Используйте стандартные методы класса String.";
+        Console.WriteLine("Введите предложение, для которого будет подсчитана средняя длина слова. ");
+        Console.WriteLine("По умолчанию <{0}>", poems);
+        string str = Console.ReadLine();
+        if (str != "") poems = str;
+        char[] div = { ' ', '.', ',', '?', '!' }; //создаем массив разделителей
+        
+        // Разбиваем строку на части
+        string[] parts = poems.Split(div, StringSplitOptions.RemoveEmptyEntries);
+        string whole = String.Join("", parts);
+
+        if (parts.Length>0) Console.WriteLine("Средняя длина слова: "+(whole.Length / parts.Length));
+    }
+}
+
+class Task12_DoubleTrouble
+{
+    public static void DoIt()
+    {
+        string str1 = "написать программу, которая";
+        string str2 = "описание";
+        string str = string.Empty;
+
+        Console.WriteLine("Введите первую строку, по умолчанию <{0}>", str1);
+        str = Console.ReadLine();
+        if (str != "") str1 = str;
+
+        Console.WriteLine("Введите вторую строку, по умолчанию <{0}>", str2);
+        str = Console.ReadLine();
+        if (str != "") str2 = str;
+
+        StringBuilder strResult = new StringBuilder(str1);
+
+        for (int i=0; i < str2.Length; i++)
+        {
+            string charChange = str2.Substring(i, 1);
+            strResult = strResult.Replace(charChange, charChange + charChange);
+        }
+
+        Console.WriteLine("Результирующая строка: "+ strResult);
+    }
+}
+
+class Task13_VincentAndJules
+{
+    public static void DoIt()
+    {
+        string str = "";
+        StringBuilder sb = new StringBuilder();
+
+        Stopwatch stopWatch = new Stopwatch();
+
+        int N = 1;
+        int Max = 1000000;
+
+        while (N<=Max)
+        {
+            Console.Write("{0, 15}  ", N);
+
+            stopWatch.Start();
+
+            for (int i = 0; i < N; i++)
+            {
+                str += "*";
+            }
+
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds);
+            Console.Write("" + elapsedTime + "  ");
+
+            stopWatch.Reset();
+
+            stopWatch.Start();
+
+            for (int i = 0; i < N; i++)
+            {
+                sb.Append("*");
+            }
+
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            ts = stopWatch.Elapsed;
+            // Format and display the TimeSpan value.
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds);
+            Console.Write("  " + elapsedTime);
+
+            Console.WriteLine();
+
+            N = N * 10;
+        }
+    }
+}
+
