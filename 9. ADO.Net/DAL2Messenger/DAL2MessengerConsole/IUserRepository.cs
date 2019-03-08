7767abe36b1ac097fe761dfece0087aa9b1157d9
alexandr.sharshavin <8593228@mail.com>
2019-03-08 18:54:57 +0400

    done. DAL2Messenger

---
+++ 9. ADO.Net/DAL2Messenger/DAL2MessengerConsole/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL2Messenger
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+        List<User> GetAllUsers(int top);
+    }
+}
---
+++ 9. ADO.Net/DAL2Messenger/WebApplication/Global.asax.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using DAL2Messenger;
+using Ninject;
+using System.Configuration;
+
+namespace WebApplication
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            DependencyResolver.SetResolver(new NinjectDependencyResolver(new StandardKernel()));
+        }
+
+        public class NinjectDependencyResolver : IDependencyResolver
+        {
+            private IKernel kernel;
+
+            public NinjectDependencyResolver(IKernel kernelParam)
+            {
+                this.kernel = kernelParam;
+                this.AddBindings();
+            }
+
+            public object GetService(Type serviceType)
+            {
+                return this.kernel.TryGet(serviceType);
+            }
+
+            public IEnumerable<object> GetServices(Type serviceType)
+            {
+                return this.kernel.GetAll(serviceType);
+            }
+
+            private void AddBindings()
+            {
+                var messengerConnectionString = "MessengerConection";
+                this.kernel.Bind<IUserRepository>().To<UserRepository>().WithConstructorArgument("connectionStringItem", ConfigurationManager.ConnectionStrings[messengerConnectionString]);
+            }
+        }
+    }
+}
